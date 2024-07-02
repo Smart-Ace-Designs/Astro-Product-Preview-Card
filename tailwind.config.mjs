e737045b9e1b89d6d82d838d1907cf141dd4dc2d
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2024-07-01 20:58:00 -0500

    Add initial styles, colors and fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryDarkCyan: "hsl(158, 36%, 37%)",
+        primaryCream: "hsl(30, 38%, 92%)",
+        secondaryVeryDarkBlue: "hsl(212, 21%, 14%)",
+        secondaryDarkGrayishBlue: "hsl(228, 12%, 48%)",
+        secondaryWhite: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "Sans-Serif"],
+        fraunces: ["Fraunces", "Sans-Serif"],
+      },
+    },
   },
   plugins: [],
 };
