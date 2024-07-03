e87383d44ec3b178698a51c77be26f9456100388
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2024-07-02 21:06:15 -0500

    Change anchor to button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,7 @@ export default {
       colors: {
         primaryDarkCyan: "hsl(158, 36%, 37%)",
         primaryCream: "hsl(30, 38%, 92%)",
+        secondaryDarkCyan: "hsl(158, 36%, 18%)",
         secondaryVeryDarkBlue: "hsl(212, 21%, 14%)",
         secondaryDarkGrayishBlue: "hsl(228, 12%, 48%)",
         secondaryWhite: "hsl(0, 0%, 100%)",
