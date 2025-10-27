65acbbeee3b0f39b6078c4c631125931fb12aa96
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-10-26 20:45:06 -0500

    Updates project to use experimental Astro fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -9,4 +9,18 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   integrations: [],
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Fraunces",
+        cssVariable: "--font-fraunces",
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Montserrat",
+        cssVariable: "--font-montserrat",
+      },
+    ],
+  },
 });
